bd98a4b798dd418323ee206b9674b51285d2c87c
Vasu Eranki <vasueranki@gmail.com>
2020-03-10 04:17:34 +0530

    Update Day_0_1.py
    Weighted_Mean

--- Day_0_1.py
+++ Day_0_1.py
@@ -1,16 +1,16 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 import os 
-def weighted_mean(n,x,w):
-    total =0
-    for i in range(0,n):
-        total = total+x[i]*w[i]
-    total = total/sum(w)
+def weighted_mean(x,w):
+    return x*w
+
     return total
 if __name__=='__main__':
     fptr = open(os.environ['OUTPUT_PATH'],'w')
     n=int(input())
     x = list(map(int,input().rstrip().split()))
     w = list(map(int,input().rstrip().split()))
-    op = weighted_mean(n,x,w)
-    fptr.write(str(op)+'/n')
+    op = list(map(weighted_mean,x,w))
+    op = sum(op)/sum(w)
+    op=round(op,1)
+    fptr.write(str(op)+'\n')
     fptr.close()
